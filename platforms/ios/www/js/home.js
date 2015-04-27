65eab9eaade724975d65910f2c07a65859adc88f
cmarkham20 <cmarkham@Chris-Markhams-iMac.local>
2015-04-27 15:06:19 -0400

    2nd Upload

--- platforms/ios/www/js/home.js
+++
@@ -1,9 +0,0 @@
-$('#home').on('pageinit', function() {
-	$("#link-ultra-family").click(function() {
-		$("#nav-ultra-family").slideToggle();
-	});
-});
-
-$(document).on('pagebeforeshow', "#home", function (event, data) {
-    $("#nav-ultra-family").hide();	
-});
